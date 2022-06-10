6cf6e0a91c47ce52f69a1c2ce1395555f839c9aa
Mohamed <mohamed.h.abukar@gmail.com>
2022-06-10 15:14:03 -0500

    Entity class made for employee table.

--- restaurantapp/src/main/java/org/mohamedabukar/restaurantapp/entity/Employee.java
+++ restaurantapp/src/main/java/org/mohamedabukar/restaurantapp/entity/Employee.java
@@ -1,14 +1,21 @@
-package org.mohamedabukar.restaurantapp.org.mohamedabukar.restaurantapp.entity;
+package org.mohamedabukar.restaurantapp.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name =  "employees")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String department;
 }
