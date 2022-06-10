262682fbd76e14d8de9a3eada92bcf673f1d27c1
Mohamed <mohamed.h.abukar@gmail.com>
2022-06-10 13:55:11 -0500

    initail commit -m

---
+++ restaurantapp/src/main/java/org/mohamedabukar/restaurantapp/RestaurantappApplication.java
@@ -0,0 +1,13 @@
+package org.mohamedabukar.restaurantapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantappApplication.class, args);
+	}
+
+}
---
+++ restaurantapp/src/test/java/org/mohamedabukar/restaurantapp/RestaurantappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mohamedabukar.restaurantapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
